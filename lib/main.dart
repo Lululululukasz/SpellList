8bb6806e435ca41d2b073512ce67c50a77bf95c3
Gultendier <lukasz.szupka@t.online.de>
2022-10-17 14:03:22 +0200

    Fixed smaller Problems, removed Buttons from main

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ void main() {
       routes: {
         // When navigating to the "/" route, build the FirstScreen widget.
         '/': (context) => const MainMenu(),
-        // When navigaconstting to the "/second" route, build the SecondScreen widget.
+        // When navigating to the "/second" route, build the SecondScreen widget.
         '/sheet': (context) =>  const Sheet(),
         '/spells': (context) => const SpellList(),
       },
@@ -34,34 +34,25 @@ class MainMenu extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Main Menu'),
       ),
-      drawer: Drawer(
+      drawer: Drawer (
 
-          child:  ListView(
-              padding: EdgeInsets.all(10),
+          child:  ListView (
+
               children: [
-                Container(
-                  height: 50,
-                  width: 50,
-                  color: Colors.blue,
-                  child:
-                  ElevatedButton(
-                    // Within the `FirstScreen` widget
-                    onPressed: () {
-                      // Navigate to the second screen using a named route.
-                      Navigator.pushNamed(context, '/sheet');
-                    },
-                    child: const Text('Characters'),
-                  ),
 
+                ElevatedButton(
+                  // Within the `FirstScreen` widget
+                  onPressed: () {
+                    // Navigate to the second screen using a named route.
+                    Navigator.pushNamed(context, '/sheet');
+                  },
+                  child: const Text('Characters'),
                 ),
-                Container(
-                  child:
-                  ElevatedButton(
-                    onPressed:(){
-                      Navigator.pushNamed(context, '/spells');
-                    },
-                    child: const Text('Spells'),
-                  ),
+                ElevatedButton(
+                  onPressed:(){
+                    Navigator.pushNamed(context, '/spells');
+                  },
+                  child: const Text('Spells'),
                 )
               ],
             )
@@ -69,26 +60,9 @@ class MainMenu extends StatelessWidget {
 
       ),
 
-      body: Center(
-          child: Column(
-            children: [
-              ElevatedButton(
-                // Within the `FirstScreen` widget
-                onPressed: () {
-                  // Navigate to the second screen using a named route.
-                  Navigator.pushNamed(context, '/sheet');
-                },
-                child: const Text('Characters'),
-              ),
-              ElevatedButton(
-                onPressed:(){
-                  Navigator.pushNamed(context, '/spells');
-                },
-                child: const Text('Spells'),
-              ),
-            ],
-          )
-      ),
+      body: const Center(
+        child: Text(''),
+      )
     );
   }
 }
