4ee4554b9c39e57c96a6717bdc92080a1f17fa23
Lukasz <Lukasz.Szupka@student.fh-kiel.de>
2022-12-15 23:04:38 +0100

    updated README.md and refactoring futureCharSelecter.dart

--- README.md
+++ README.md
@@ -4,8 +4,11 @@ Our MOB Project DnD-App
 
 ## Introduction
 
-This project is a aplication to support Dungeons and Dragons 5th Editon
+This project is a application to support Dungeons and Dragons 5th Edition
 
-You will have the possability to create character sheets and look for spells for your specific class.
+You will have the possibility to create character sheets and look for spells for your specific class.
+
+The reason why we don't use Android Version 9:
+https://www.cvedetails.com/vulnerability-list/vendor_id-1224/product_id-19997/version_id-602087/Google-Android-9.0.html
 
 WIP.
--- lib/CharacterCreater/futureCharSelecter.dart
+++ lib/CharacterCreater/futureCharSelecter.dart
@@ -1,6 +1,5 @@
 import '../Firebase/collectionForCharacter.dart';
 import 'package:flutter/material.dart';
-import 'characteCreater.dart';
 
 class FutureCharacterSelection extends StatelessWidget {
   const FutureCharacterSelection({Key? key}) : super(key: key);
@@ -10,35 +9,36 @@ class FutureCharacterSelection extends StatelessWidget {
     getChar();
     return FutureBuilder(
       future: getChar(),
-      builder: (context,snapshot){
-        if (snapshot.hasError){
-          return Text (snapshot.error.toString());
-        } else if(snapshot.hasData){
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Text(snapshot.error.toString());
+        } else if (snapshot.hasData) {
           List<ListTile> CharacterTiles = [];
-         List<String> chars = snapshot.data as List<String>;
-         chars.forEach((element) {
-           CharacterTiles.add(
-               ListTile(
-                  leading: Text(element),
-                  shape: const Border(bottom: BorderSide(), top: BorderSide()),
-           ));
-         }
-         );
+          List<String> chars = snapshot.data as List<String>;
+          chars.forEach((element) {
+            CharacterTiles.add(ListTile(
+              leading: Text(element),
+              shape: const Border(bottom: BorderSide(), top: BorderSide()),
+            ));
+          });
           return ListView(
             children: CharacterTiles,
           );
-
         } else {
-          return const Center(child: CircularProgressIndicator(),);
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
         }
       },
     );
   }
-  Future getChar () async{
+
+  Future getChar() async {
     List<String> characterName = [];
     final snapshot = await db.collection("Character").get();
-    snapshot.docs.forEach((element) {characterName.add(element.data().values.first);}
-    );
+    snapshot.docs.forEach((element) {
+      characterName.add(element.data().values.first);
+    });
     return characterName;
   }
 }
