db18d2556388d02542e588ae4135cebabb389568
Lukasz <Lukasz.Szupka@student.fh-kiel.de>
2022-12-16 12:32:46 +0100

    renamed menu

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:mob_project/Register/register.dart';
-import 'menu.dart';
+import 'CharacterCreator/charactorCreatorMain.dart';
 import 'spellList.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
 import 'package:firebase_core/firebase_core.dart';
