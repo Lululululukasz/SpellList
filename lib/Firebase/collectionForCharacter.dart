42753307bf85e1a5e968fc9569d183266f386068
janoleweighardt <jan.o.weighardt@student.fh-kiel.de>
2022-12-16 01:38:49 +0100

    Registrierung2.0

---
+++ lib/Firebase/collectionForCharacter.dart
@@ -0,0 +1,39 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+final FirebaseFirestore db = FirebaseFirestore.instance;
+class Character_stats
+{
+  final String? characterName;
+  final String? characterClasses;
+  final int? level;
+
+
+
+  Character_stats({
+    this.characterName,
+    this.characterClasses,
+    this.level,
+
+  });
+
+
+  factory Character_stats.fromFirestore(
+      DocumentSnapshot<Map<String, dynamic>> snapshot,
+      SnapshotOptions? options,
+      ) {
+    final data = snapshot.data();
+    return Character_stats(
+      characterName: data?['name'],
+      characterClasses: data?['classes'],
+      level: data?['level'],
+    );
+  }
+
+  Map<String, dynamic> toFirestore() {
+    return {
+      if (characterName != null) "name": characterName,
+      if (characterClasses != null) "classes": characterClasses,
+      if (level != null) "level": level,
+    };
+  }
+}
